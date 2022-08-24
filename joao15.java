40b791944203bc10776032c5dd055f95adbb0bfd
joaofeliz14 <110936545+tiagoreisdosanaos@users.noreply.github.com>
2022-08-24 14:53:53 -0300

    Create joao15.java

---
+++ joao15.java
@@ -0,0 +1,23 @@
+public class carro 3 {
+   
+        public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+       
+        int carros = 0;
+        double salario = 1000;
+       
+        System.out.println("Qual a quantidade de carros vendidos pelo funcionario: ");
+        carro 3 = entrada.nextInt();
+       
+        if(carros>5){
+            salario = carro 3*1000;
+        }
+        else{
+            salario = carro 3*500;
+        }
+       
+        salario = salario + 1000;
+       
+        System.out.println("O salario total do funcionario é: "+ salario);
+ 
+        }
